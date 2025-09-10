2c141bf714f12458747ce494a67c5d19560cb204
tusharchaudharryy <chaudharytushar477@gmail.com>
2025-09-11 02:12:59 +0530

    Final Commits

--- setup.py
+++ setup.py
@@ -1,56 +1,39 @@
-<<<<<<< HEAD
-from setuptools import setup, find_packages
+'''
+The setup.py file is an essential part of packaging and 
+distributing Python projects. It is used by setuptools 
+(or distutils in older Python versions) to define the configuration 
+of your project, such as its metadata, dependencies, and more
+'''
+
+from setuptools import find_packages,setup
 from typing import List
-HYPHEN_E_DOT = '-e .'
 
-def get_requirements(file_path:str)->List[str]:
-    '''
-    This function will return the list of requirements
-    '''
-    requirements = []
-    with open(file_path) as file_obj:
-        requirements = file_obj.readlines()
-        requirements = [req.replace('\n', '') for req in requirements]
+def get_requirements()->List[str]:
+    """
+    Thiss function will return list of requirements
+    
+    """
+    requirement_lst:List[str]=[]
+    try:
+        with open('requirements.txt','r') as file:
+            #Read lines from the file
+            lines=file.readlines()
+            ## Process each line
+            for line in lines:
+                requirement=line.strip()
+                ## ignore empty lines and -e .
+                if requirement and requirement!= '-e .':
+                    requirement_lst.append(requirement)
+    except FileNotFoundError:
+        print("requirements.txt file not found")
 
-        if HYPHEN_E_DOT in requirements:
-            requirements.remove(HYPHEN_E_DOT)
-
-    return requirements
+    return requirement_lst
 
 setup(
-    name='ML_Project',
-    version='0.0.1',
-    author='Tushar Chaudhary',
-    author_email='chaudharytushar477@gmail.com',
+    name="NetworkSecurity",
+    version="0.0.1",
+    author="Divyansh Bansal",
+    author_email="divyanshb30@gmail.com",
     packages=find_packages(),
-    install_requires=get_requirements('requirements.txt')
-
-=======
-from setuptools import setup, find_packages
-from typing import List
-HYPHEN_E_DOT = '-e .'
-
-def get_requirements(file_path:str)->List[str]:
-    '''
-    This function will return the list of requirements
-    '''
-    requirements = []
-    with open(file_path) as file_obj:
-        requirements = file_obj.readlines()
-        requirements = [req.replace('\n', '') for req in requirements]
-
-        if HYPHEN_E_DOT in requirements:
-            requirements.remove(HYPHEN_E_DOT)
-
-    return requirements
-
-setup(
-    name='ML_Project',
-    version='0.0.1',
-    author='Tushar Chaudhary',
-    author_email='chaudharytushar477@gmail.com',
-    packages=find_packages(),
-    install_requires=get_requirements('requirements.txt')
-
->>>>>>> 568bd63 (New Commits)
+    install_requires=get_requirements()
 )
