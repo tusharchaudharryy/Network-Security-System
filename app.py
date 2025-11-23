525bebab07eef8b5282b4b6b6b2e2c2425caf7ea
tusharchaudharryy <chaudharytushar477@gmail.com>
2025-11-24 01:27:54 +0530

    FASTAPI implementation

--- app.py
+++ app.py
@@ -73,12 +73,9 @@ async def predict_route(request: Request,file: UploadFile = File(...)):
         print(y_pred)
         df['predicted_column'] = y_pred
         print(df['predicted_column'])
-        #df['predicted_column'].replace(-1, 0)
-        #return df.to_json()
         os.makedirs("prediction_output", exist_ok=True)
         df.to_csv('prediction_output/output.csv')
         table_html = df.to_html(classes='table table-striped')
-        #print(table_html)
         return templates.TemplateResponse("table.html", {"request": request, "table": table_html})
         
     except Exception as e:
