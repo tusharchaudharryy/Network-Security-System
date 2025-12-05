08028f25e62c28b840bcc7ea75b801b850baeff2
tusharchaudharryy <chaudharytushar477@gmail.com>
2025-12-05 18:22:49 +0530

    update: new data transformation technique

--- networksecurity/components/data_transformation.py
+++ networksecurity/components/data_transformation.py
@@ -87,8 +87,5 @@ class DataTransformation:
                 transformed_test_file_path=self.data_transformation_config.transformed_test_file_path
             )
             return data_transformation_artifact
-
-
-            
         except Exception as e:
             raise NetworkSecurityException(e,sys)
--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -3,9 +3,6 @@ import sys
 import numpy as np
 import pandas as pd
 
-"""
-defining common constant variable for training pipeline
-"""
 TARGET_COLUMN = "Result"
 PIPELINE_NAME: str = "NetworkSecurity"
 ARTIFACT_DIR: str = "Artifacts"
@@ -18,9 +15,6 @@ SCHEMA_FILE_PATH = os.path.join("data_schema", "schema.yaml")
 SAVED_MODEL_DIR =os.path.join("saved_models")
 MODEL_FILE_NAME = "model.pkl"
 
-"""
-Data Ingestion related constant start with DATA_INGESTION VAR NAME
-"""
 DATA_INGESTION_COLLECTION_NAME: str = "NetworkSecurityData"
 DATA_INGESTION_DATABASE_NAME: str = "DIVYANSHAI"
 DATA_INGESTION_DIR_NAME: str = "data_ingestion"
@@ -29,9 +23,6 @@ DATA_INGESTION_INGESTED_DIR: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATION: float = 0.2
 
 
-"""
-Data Validation related constant start with DATA_VALIDATION VAR NAME
-"""
 DATA_VALIDATION_DIR_NAME: str = "data_validation"
 DATA_VALIDATION_VALID_DIR: str = "validated"
 DATA_VALIDATION_INVALID_DIR: str = "invalid"
@@ -39,14 +30,10 @@ DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
 DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
 
 
-"""
-Data Transformation related constant start with DATA_TRANSFORMATION VAR NAME
-"""
 DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation"
 DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = "transformed"
 DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR: str = "transformed_object"
 
-## kkn imputer to replace nan values
 DATA_TRANSFORMATION_IMPUTER_PARAMS: dict = {
     "missing_values": np.nan,
     "n_neighbors": 3,
@@ -57,9 +44,6 @@ DATA_TRANSFORMATION_TRAIN_FILE_PATH: str = "train.npy"
 DATA_TRANSFORMATION_TEST_FILE_PATH: str = "test.npy"
 PREPROCESSING_OBJECT_FILE_NAME = "preprocessing.pkl"
 
-"""
-Model Trainer ralated constant start with MODE TRAINER VAR NAME
-"""
 
 MODEL_TRAINER_DIR_NAME: str = "model_trainer"
 MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"
